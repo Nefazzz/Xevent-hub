c38b969c1c90ea1c09ba037d5b24728ee3ab8b90
Nefazzz <69803816+Nefazzz@users.noreply.github.com>
2024-02-17 16:14:35 +0700

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -8,22 +8,6 @@
  
  
   ---------Config
-_G.antiscan = true
-
-getgenv().A = require(game:GetService("ReplicatedStorage").CombatFramework.RigLib).wrapAttackAnimationAsync
-
-getgenv().B = require(game.Players.LocalPlayer.PlayerScripts.CombatFramework.Particle).play
-_G.setfflag = true
-
-spawn(function()
-    while wait() do
-        if _G.setfflag then
-            setfflag("AbuseReportScreenshot", "False")
-            setfflag("AbuseReportScreenshotPercentage", "0")
-        end
-    end
-end)
-  
   if not getgenv().Config then
       getgenv().Config = {
           autofarmlevel = false,
